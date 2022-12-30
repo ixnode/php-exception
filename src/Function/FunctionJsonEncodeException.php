fb3f99fe8b31af3a753c57eb1c8c3c67de486fdc
Björn Hempel <bjoern@hempel.li>
2022-12-30 18:58:32 +0100

    Add FunctionJsonEncodeException

---
+++ src/Function/FunctionJsonEncodeException.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the ixno/php-exception project.
+ *
+ * (c) Björn Hempel <https://www.hempel.li/>
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+namespace Ixnode\PhpException\Function;
+
+use Ixnode\PhpException\Function\Base\BaseFunctionException;
+
+/**
+ * Class FunctionJsonEncodeException
+ *
+ * @author Björn Hempel <bjoern@hempel.li>
+ * @version 0.1.0 (2022-12-30)
+ * @since 0.1.0 (2022-12-30) First version.
+ */
+final class FunctionJsonEncodeException extends BaseFunctionException
+{
+    public const TEXT_PLACEHOLDER = 'Unable to encode the given object into a JSON string.';
+
+    /**
+     * JsonEncodeException constructor.
+     *
+     */
+    public function __construct()
+    {
+        $messageNonVerbose = self::TEXT_PLACEHOLDER;
+
+        parent::__construct($messageNonVerbose);
+    }
+}
